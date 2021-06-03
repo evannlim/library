7575ab53532d4ac52ea4962283d5ef69163b2081
evannlim <evann@piorsoft.com>
2021-06-03 13:35:22 -0700

    Changed Program.cs

--- Library.App/Program.cs
+++ Library.App/Program.cs
@@ -20,7 +20,6 @@ namespace Library.App
                 {
                     case "show":
                         bookshelf.ShowBookshelf();
-                        Console.WriteLine();
                         break;
                     case "add":
                         Console.WriteLine("Type in the title of the book:");
